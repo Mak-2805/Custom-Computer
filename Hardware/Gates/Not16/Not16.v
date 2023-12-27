36b9679f86d9283216aed69c6e9beaf735fe7964
Mak-2805 <makariossamwail@gmail.com>
2023-12-27 04:59:54 -0500

    Implemented the 16 bit Not gate

---
+++ Hardware/Gates/Not16/Not16.v
@@ -0,0 +1,17 @@
+module Not16 (in,out);
+	
+	input [15:0] in;
+	output [15:0] out;
+
+  
+	generate
+		genvar i;
+		for (i = 0; i < 16; i = i + 1) begin : instance_loop
+			oneNot Not_inst (
+			  .a(in[i]),
+			  .out(out[i])
+			);
+		end
+	endgenerate
+  
+endmodule
